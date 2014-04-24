14cea1639b5f52676561fb7feae97e66b8d5f991
Tyler Hartland <tyler.hartland@code42.com>
2014-04-24 17:27:31 -0500

    test version of checking for downward merges

--- lib/sc.rb
+++ lib/sc.rb
@@ -1,1 +1,46 @@
-module SC; end
+module SC
+  BRANCHES = {
+    master: nil,
+    hotfix: {
+      branches_from: 'master',
+      merges_to: 'master',
+      prefix: 'hotfix',
+      semantic_level: 'patch'
+    },
+    release: {
+      branches_from: 'develop',
+      merges_to: 'master',
+      prefix: 'release',
+      semantic_level: 'minor'
+    },
+    develop: nil
+  }
+
+  def self.check
+    report = []
+    branches.each_with_index do |branch, i|
+      branches[(i + 1)..-1].each do |superset|
+        unless branch.subset_of?(superset)
+          report << "#{branch} should be merged into #{superset}"
+        end
+      end
+    end
+
+    if report.empty?
+      puts 'No merges needed.'
+    else
+      puts report.join("\n")
+    end
+  end
+
+  def self.branches
+    @branches ||= BRANCHES.map do |k, v|
+      if v
+        sc_branch = SC::Branch.new(k) if v
+        sc_branch.latest if sc_branch.unmerged?
+      else
+        SC::Git::Branch.new(k)
+      end
+    end.compact
+  end
+end
