a6eeb126e90cd7bf54493939e815bc6e55a0b8c3
Tyler Hartland <tyler.hartland@code42.com>
2014-04-25 12:44:13 -0500

    src check reports if local branches are not up to date with remotes

--- lib/src/git.rb
+++ lib/src/git.rb
@@ -1,5 +1,7 @@
-module SRC::Git
-  def self.fetch
-    raise 'fetch failed' unless system('git fetch -q')
+module SRC
+  module Git
+    def self.fetch
+      raise 'fetch failed' unless system('git fetch -q')
+    end
   end
 end
